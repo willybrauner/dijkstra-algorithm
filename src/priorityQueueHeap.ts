577f97639228f4b0f78e489766e33397370ef9d2
Willy Brauner <mail@willybrauner.com>
2022-06-29 11:56:21 +0200

    Start remove

--- src/priorityQueueHeap.ts
+++ src/priorityQueueHeap.ts
@@ -2,11 +2,12 @@ const { log } = console
 
 /**
  * Priority queue with Heap
+ *
  * Goal is not to maintain an update a strict order list
  * but to be sure that:
  *  - (nodeIndex * 2) & (nodeIndex * 2 + 1) are smaller (or greater) than nodeIndex priority in heap
- *  - floor(nodeIndex / 2) is bigger than 
- * 
+ *  - floor(nodeIndex / 2) is bigger than
+ *
  * heap example after insertion:
  *     [
  *       null,
@@ -16,7 +17,7 @@ const { log } = console
  *       { key: 'A', priority: 100 },
  *       { key: 'C', priority: 30 }
  *     ]
- * 
+ *
  * @doc: https://en.wikipedia.org/wiki/Heap_(data_structure)
  */
 export function priorityQueueHeap<T>(type: "min" | "max" = "min") {
@@ -24,10 +25,8 @@ export function priorityQueueHeap<T>(type: "min" | "max" = "min") {
   /**
    * enqueue
    * Insert value is priority in appropriate position in heap
-   * @param key
-   * @param priority
    */
-  const enqueue = (key: any, priority: number): void => {
+  const enqueue = (key: string, priority: number): void => {
     const node = { key, priority }
     // push new node in heap (at the end of heap)
     // get new current node index (at the last position of heap array)
@@ -36,10 +35,10 @@ export function priorityQueueHeap<T>(type: "min" | "max" = "min") {
     let nodeIndex = heap.length - 1
     let parentNodeIndex = Math.floor(nodeIndex / 2)
 
-    while ( 
+    while (
       type === "min"
-      ? node.priority < heap?.[parentNodeIndex]?.priority
-      : node.priority > heap?.[parentNodeIndex]?.priority
+        ? node.priority < heap?.[parentNodeIndex]?.priority
+        : node.priority > heap?.[parentNodeIndex]?.priority
     ) {
       // invert parent node and node in heap
       // update there indexs
@@ -47,7 +46,7 @@ export function priorityQueueHeap<T>(type: "min" | "max" = "min") {
       heap[parentNodeIndex] = node
       heap[nodeIndex] = parentNode
       nodeIndex = parentNodeIndex
-      parentNodeIndex = Math.floor(nodeIndex / 2)    
+      parentNodeIndex = Math.floor(nodeIndex / 2)
     }
 
     // log(heap)
@@ -55,33 +54,38 @@ export function priorityQueueHeap<T>(type: "min" | "max" = "min") {
 
   /**
    * dequeue
-   * Remove item from heap 
-   * 
-   * @returns
+   * Remove item from heap
    */
   const dequeue = () => {
-    if (heap.length < 3) {
-      const toReturn = heap.pop()
-      heap[0] = null
-      return toReturn
-    }
+    // if (heap.length < 3) {
+    //   const toReturn = heap.pop()
+    //   heap[0] = null
+    //   return toReturn
+    // }
 
-    // start 1 because 0 is 'null'
-    const toRemove = heap[1]
+
+    const node = heap[1]
     heap[1] = heap.pop()
 
-    let currentIndex = 1
-    let [left, right] = [2 * currentIndex, 2 * currentIndex + 1]
-    let currentChildIndex = heap[right]?.priority <= heap[left].priority ? right : left
+    let index = 1
+    let [left, right] = [2 * index, 2 * index + 1]
+  
+    let childIndex = 
+    type === "min" 
+      ? heap[right]?.priority >= heap[left].priority ? right : left
+      : heap[right]?.priority <= heap[left].priority ? right : left
+
 
     while (
-      heap[currentChildIndex] &&
-      heap[currentIndex].priority >= heap[currentChildIndex].priority
-    ) {
-      heap[currentChildIndex] = heap[currentIndex]
-      heap[currentIndex] = heap[currentChildIndex]
+      type === "min"
+      ? heap[index].priority <= heap?.[childIndex]?.priority
+      : heap[index].priority >= heap?.[childIndex]?.priority
+    ){
+      heap[childIndex] = heap[index]
+      heap[index] = heap[childIndex]
     }
-    return toRemove
+
+    return node
   }
 
   const isEmpty = (): boolean => {
--- test/priorityQueueHeap.test.ts
+++ test/priorityQueueHeap.test.ts
@@ -1,21 +1,29 @@
 import { priorityQueueHeap } from "../src/priorityQueueHeap"
 
-it("should work with heap", () => {
-  const queue = priorityQueueHeap("min")
-  queue.enqueue("A", 100)
-  queue.enqueue("A", 30)
-  queue.enqueue("A", 29)
-  queue.enqueue("A", 27)
-  queue.enqueue("A", 24)
-  queue.enqueue("A", 20)
-  queue.enqueue("A", 19)
-  queue.enqueue("A", 17)
-  queue.enqueue("A", 12)
+const queue = priorityQueueHeap("min")
 
+it("should insert item properly in heap", () => {
+  ;[100, 30, 29, 27, 24, 20, 19, 17, 12].forEach((e) => {
+    queue.enqueue("A", e)
+  })
   console.log(queue.heap)
 
   for (let index = 1; index < Math.floor(queue.heap.length / 2); index++) {
     expect(queue.heap[index * 2].priority).toBeGreaterThan(queue.heap[index].priority)
     expect(queue.heap[index * 2 + 1].priority).toBeGreaterThan(queue.heap[index].priority)
   }
+})
+
+it("should remove item properly from heap", () => {
+  
+  // last item become the first  
+  const firstHeapItem = queue.heap[1]
+  const item = queue.dequeue()
+  expect(item.priority).toBe(firstHeapItem.priority)
+  console.log(queue.heap)
+
+//   for (let index = 1; index < Math.floor(queue.heap.length / 2); index++) {
+//       expect(queue.heap[index * 2].priority).toBeGreaterThan(queue.heap[index].priority)
+//       expect(queue.heap[index * 2 + 1].priority).toBeGreaterThan(queue.heap[index].priority)
+//     }
 })
